4a9d78bc32c24336537811f23708dc25b23b089d
Erwan GUEGANIC <GUEGANIC_Er@lprs.local>
2024-05-16 16:32:29 +0200

    laravel push

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Laravel\Prompts\Key;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->interger('ref_evenement')->primary();
+            $table->interger('ref_user')->primary();
+            $table->foreign('ref_evenement')->references('id_evenement')->on('evenement');
+            $table->foreign('ref_user')->references('id')->on('users');
+        });
+
+        Schema::create('role', function (Blueprint $table) {
+            $table->interger('id_role')->primary();
+            $table->string('libelle', 255)->nullable();
+        });
+
+        Schema::create('evenement', function (Blueprint $table) {
+            $table->id_evenement()->primary();
+            $table->string('titre', 45)->nullable();
+            $table->text('description')->nullable();
+            $table->dateTime('date');
+            $table->int('place', 1000);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+ROUTE::get('/reservation', function () {
+    return view('reservation');
+})->middleware(['auth', 'verified'])->name('reservation');
+
+ROUTE::get('/historique', function () {
+    return view('historique');
+})->middleware(['auth', 'verified'])->name('historique');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
